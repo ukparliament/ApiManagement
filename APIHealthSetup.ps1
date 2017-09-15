9c8a4149c484cb8237fe9c11a6911282b3090f33
hellikopter <hellikopter@users.noreply.github.com>
2017-09-15 12:39:04 +0100

    sinigle cluster deployment changes;
    Related Work Items: #348

---
+++ APIHealthSetup.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+Generates API to check availability of the API Management.
+
+.DESCRIPTION
+Generates API to check availability of the API Management.
+
+.PARAMETER APIResourceGroupName
+Name of the Resource Group where the API Management is.
+
+.PARAMETER APIManagementName
+Name of the API Management.
+
+.NOTES
+This script is for use as a part of deployment in VSTS only.
+#>
+
+Param(
+    [Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
+    [Parameter(Mandatory=$true)] [string] $APIManagementName
+)
+$ErrorActionPreference = "Stop"
+
+function Log([Parameter(Mandatory=$true)][string]$LogText){
+    Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
+}
+
+Log "Get API Management context"
+$management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $APIManagementName
+
+Log "Create API Health"
+$serviceUrl="https://$APIManagementName.portal.azure-api.net"
+$apiHealth=Get-AzureRmApiManagementApi -Context $management | where ServiceUrl -EQ $serviceUrl
+if ($apiHealth -eq $null) {
+	apiHealth=New-AzureRmApiManagementApi -Context $management -Name "API Health" -ServiceUrl $serviceUrl -Protocols @("https") -Path "/api-health"
+	$operationAPIHealth=New-AzureRmApiManagementOperation -Context $management -ApiId $apiHealth.ApiId -Name "Get" -Method "GET" -UrlTemplate "/*"
+	Set-AzureRmApiManagementPolicy -Context $management -ApiId $apiHealth.ApiId -OperationId $operationAPIHealth.OperationId -PolicyFilePath "$PoliciesFolderLocation\APIHealthGET.xml"
+}
+
+Log "Create API product"
+$apiProduct=New-AzureRmApiManagementProduct -Context $management -Title "Parliament - API Availability" -Description "For parliamentary use only." -ApprovalRequired $true -SubscriptionsLimit 1 -SubscriptionRequired $true
+Add-AzureRmApiManagementApiToProduct -Context $management -ProductId $apiProduct.ProductId -ApiId $apiHealth.ApiId
+
+Log "Job well done!"
